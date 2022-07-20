a7f13c0c5c55e8fb8e25d62fd925d9befd517271
Lyncohn <lyncohn.macedoz@gmail.com>
2022-07-20 14:11:25 -0300

    Commit

---
+++ src/empresa/controller/new-empresa.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    $('.btn-new').click(function(e) {
+        e.preventDefault()
+
+        $('.modal-title').empty()
+        $('.modal-body').empty()
+
+        $('modal-title').append('Adicionar uma nova empresa')
+
+        $('modal-body').load('src/empresa/view/form-empresa.html')
+
+        $('#modal-empresa').modal('show')
+    })
+})
